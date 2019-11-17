a6194e9d6bf602682bd3a53ea1bb0d20e7d59a33
quoctoanpk2511 <quoctoanpk2511@gmail.com>
2019-11-17 23:15:00 +0700

    week7

--- toy_app/db/schema.rb
+++ toy_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_17_114153) do
+ActiveRecord::Schema.define(version: 2019_11_17_160706) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_11_17_114153) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
   end
 
 end
